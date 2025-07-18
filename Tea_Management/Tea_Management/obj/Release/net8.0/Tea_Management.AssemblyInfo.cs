87ec319ad2b8b05d32cf99d4f1793e0336053c83
Chamika Thennakoon <thennakoonchamika11@gmail.com>
2025-07-18 11:20:40 +0530

    v1.1

--- Tea_Management/Tea_Management/obj/Release/net8.0/Tea_Management.AssemblyInfo.cs
+++ Tea_Management/Tea_Management/obj/Release/net8.0/Tea_Management.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tea_Management")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b002955d63fe55a11dc21af48fc7f5081aeb769")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tea_Management")]
